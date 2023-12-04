e10a8fd3c62e136e2d7a49daa5e1a868740b320b
mikey <spannmikey@gmail.com>
2023-12-03 23:35:57 -0600

    change sound and some data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,31 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
-import { logo } from "../assets/images";
+import { logo } from '../assets/images';
 
 const Navbar = () => {
-  return (
-    <header className='header'>
-      <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
-      </NavLink>
-      <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
-          About
-        </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
-          Projects
-        </NavLink>
-      </nav>
-    </header>
-  );
+	return (
+		<header className='header'>
+			<NavLink to='/'>
+				<p className='w-18 h-18 blue-gradient_text'>MS</p>
+			</NavLink>
+			<nav className='flex text-lg gap-7 font-medium'>
+				<NavLink
+					to='/about'
+					className={({ isActive }) =>
+						isActive ? 'blue-gradient_text' : 'text-black'
+					}>
+					About
+				</NavLink>
+				<NavLink
+					to='/projects'
+					className={({ isActive }) =>
+						isActive ? 'blue-gradient_text' : 'text-black'
+					}>
+					Projects
+				</NavLink>
+			</nav>
+		</header>
+	);
 };
 
 export default Navbar;
--- src/constants/index.js
+++ src/constants/index.js
@@ -58,39 +58,9 @@ export const skills = [
         type: "Frontend",
     },
     {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-    {
-        imageUrl: motion,
-        name: "Motion",
-        type: "Animation",
-    },
-    {
-        imageUrl: mui,
-        name: "Material-UI",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nextjs,
-        name: "Next.js",
-        type: "Frontend",
-    },
-    {
-        imageUrl: nodejs,
-        name: "Node.js",
-        type: "Backend",
-    },
-    {
         imageUrl: react,
         name: "React",
         type: "Frontend",
-    },
-    {
-        imageUrl: redux,
-        name: "Redux",
-        type: "State Management",
     },
     {
         imageUrl: sass,
@@ -183,46 +153,36 @@ export const socialLinks = [
 ];
 
 export const projects = [
-    {
-        iconUrl: pricewise,
-        theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
-    },
-    {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
-    },
-    {
-        iconUrl: car,
-        theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-    },
-    {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
-    },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-    }
+	{
+		iconUrl: threads,
+		theme: 'btn-back-green',
+		name: 'Video Game Discovery',
+		description:
+			'Created a React application with Chakra-UI to create an application where users can view different games and filter them by category, console, and more.',
+		link: 'https://videogame-discovery.netlify.app/',
+	},
+	{
+		iconUrl: snapgram,
+		theme: 'btn-back-pink',
+		name: 'Social Media Dashboard',
+		description:
+			'Built a dashboard with HTML, SCSS, and JavaScript. This helped me understand how to design my code in a maintainable and scalable way.',
+		link: 'https://social-media-dash-mikey.netlify.app/',
+	},
+	{
+		iconUrl: pricewise,
+		theme: 'btn-back-red',
+		name: 'URL Saver Chrome Extension',
+		description:
+			'Created a chrome extension that allows users to save links from chrome browser. This was expired due to the fact that I hate having tons of bookmarks for different websites.',
+		link: 'https://github.com/spannmikey/url-saver',
+	},
+	{
+		iconUrl: estate,
+		theme: 'btn-back-black',
+		name: 'Portfolio Project',
+		description:
+			'Developed a 3d portfolio to host and share my talent across the web with future employers. Challenged me to learn how the 3d works and implement good design practices.',
+		link: 'https://mspann-portfolio.netlify.app',
+	},
 ];

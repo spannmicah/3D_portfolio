355151bf8882405bc9be098651d3a98c6fc83566
micah <micah@micahs-MacBook-Air.local>
2024-11-21 13:07:14 -0600

    fix broken links

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,129 +1,129 @@
-import { meta, shopify, starbucks, tesla } from '../assets/images';
+import { meta, shopify, starbucks, tesla } from "../assets/images";
 import {
-	car,
-	contact,
-	css,
-	estate,
-	express,
-	git,
-	github,
-	html,
-	javascript,
-	linkedin,
-	mongodb,
-	motion,
-	mui,
-	nextjs,
-	nodejs,
-	pricewise,
-	react,
-	redux,
-	sass,
-	snapgram,
-	summiz,
-	tailwindcss,
-	threads,
-	typescript,
-} from '../assets/icons';
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
 
 export const skills = [
-	{
-		imageUrl: css,
-		name: 'CSS',
-		type: 'Frontend',
-	},
-	{
-		imageUrl: git,
-		name: 'Git',
-		type: 'Version Control',
-	},
-	{
-		imageUrl: github,
-		name: 'GitHub',
-		type: 'Version Control',
-	},
-	{
-		imageUrl: html,
-		name: 'HTML',
-		type: 'Frontend',
-	},
-	{
-		imageUrl: javascript,
-		name: 'JavaScript',
-		type: 'Frontend',
-	},
-	{
-		imageUrl: react,
-		name: 'React',
-		type: 'Frontend',
-	},
-	{
-		imageUrl: sass,
-		name: 'Sass',
-		type: 'Frontend',
-	},
-	{
-		imageUrl: tailwindcss,
-		name: 'Tailwind CSS',
-		type: 'Frontend',
-	},
-	{
-		imageUrl: typescript,
-		name: 'TypeScript',
-		type: 'Frontend',
-	},
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
 ];
 
 export const socialLinks = [
-	{
-		name: 'Contact',
-		iconUrl: contact,
-		link: '/contact',
-	},
-	{
-		name: 'GitHub',
-		iconUrl: github,
-		link: 'https://github.com/spannmikey',
-	},
-	{
-		name: 'LinkedIn',
-		iconUrl: linkedin,
-		link: 'https://www.linkedin.com/in/micah-spann-84ba8b23b/',
-	},
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/spannmicah",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/micah-spann-84ba8b23b/",
+  },
 ];
 
 export const projects = [
-	{
-		iconUrl: summiz,
-		theme: 'btn-back-pink',
-		name: 'Face Detection',
-		description:
-			'Created a Face Detection using the Clarifai API face recognition AI models to detect medium to high resolution pictures with React. You can detect any person from close or afar.',
-		link: 'https://facedetectionapplication.netlify.app/',
-	},
-	{
-		iconUrl: threads,
-		theme: 'btn-back-green',
-		name: 'Video Game Discovery',
-		description:
-			'Created a React application with Chakra-UI to create an application where users can view different games and filter them by category, console, and more.',
-		link: 'https://videogame-discovery.netlify.app/',
-	},
+  {
+    iconUrl: summiz,
+    theme: "btn-back-pink",
+    name: "Splashtees",
+    description:
+      "Created a website using Next JS connected to a CMS for a client to showcase their t-shirt company while allowing them to add new products through the use of the CMS.",
+    link: "https://splashtees.net/",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Video Game Discovery",
+    description:
+      "Created a React application with Chakra-UI to create an application where users can view different games and filter them by category, console, and more.",
+    link: "https://videogame-discovery.netlify.app/",
+  },
 
-	{
-		iconUrl: pricewise,
-		theme: 'btn-back-red',
-		name: 'Etch-A-Sketch',
-		description:
-			'Created an application that allows you to sketch a drawing when hovering over the grid. Users can change the grid size and change the colors they use as they draw.',
-		link: 'https://etching-and-sketching.netlify.app/',
-	},
-	{
-		iconUrl: estate,
-		theme: 'btn-back-black',
-		name: 'Portfolio Project',
-		description:
-			'Created a 3d portfolio to host and share my talent across the web with future employers. Challenged me to learn how the 3d works and implement good design practices.',
-		link: 'https://mspann-portfolio.netlify.app',
-	},
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Etch-A-Sketch",
+    description:
+      "Created an application that allows you to sketch a drawing when hovering over the grid. Users can change the grid size and change the colors they use as they draw.",
+    link: "https://etching-and-sketching.netlify.app/",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Portfolio Project",
+    description:
+      "Created a 3d portfolio to host and share my talent across the web with future employers. Challenged me to learn how the 3d works and implement good design practices.",
+    link: "https://mspann-portfolio.netlify.app",
+  },
 ];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,69 +1,66 @@
-import { CTA } from '../components';
-import { skills } from '../constants';
+import { CTA } from "../components";
+import { skills } from "../constants";
 
 const About = () => {
-	return (
-		<section className='max-container'>
-			<h1 className='head-text'>
-				Hello, I'm{' '}
-				<span className='blue-gradient_text font-semibold drop-shadow'>
-					Micah
-				</span>
-			</h1>
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Micah
+        </span>
+      </h1>
 
-			<div className='mt-5 flex flex-col gap-3 text-slate-500'>
-				<p>
-					Front-end developer based in the US, specializing in building
-					responsive and scalable web apps with a strong focus in creating the
-					best design patterns.
-				</p>
-			</div>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Front-end developer based in the US, specializing in building
+          responsive and scalable web apps with a strong focus in creating the
+          best design patterns.
+        </p>
+      </div>
 
-			<div className='py-10 flex flex-col'>
-				<h3 className='subhead-text'>My Skills</h3>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
 
-				<div className='mt-16 flex flex-wrap gap-12'>
-					{skills.map((skill) => (
-						<div
-							className='block-container w-20 h-20'
-							key={skill.name}>
-							<div className='btn-back rounded-xl' />
-							<div className='btn-front rounded-xl flex justify-center items-center'>
-								<img
-									src={skill.imageUrl}
-									alt={skill.name}
-									className='w-1/2 h-1/2 object-contain'
-								/>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20" key={skill.name}>
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
-			<div className='py-6'>
-				<h3 className='subhead-text'>Work experience</h3>
-				<div className='mt-5 flex flex-col gap-3 text-slate-500'>
-					<p className='text-md '>
-						I've worked with companies like CSI that helped me grow and improve
-						both my character and my technical craft. Joining forces with many
-						intelligent people to get a project done. Please see my{' '}
-						<a
-							className='blue-gradient_text font-semibold cursor-pointer'
-							href='https://apricot-timothea-93.tiiny.site/'
-							target='_blank'
-							rel='no-referrer'>
-							resume {''}
-						</a>
-						for more.
-					</p>
-				</div>
-			</div>
+      <div className="py-6">
+        <h3 className="subhead-text">Work experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p className="text-md ">
+            I've worked with companies like CSI that helped me grow and improve
+            both my character and my technical craft. Joining forces with many
+            intelligent people to get a project done. If you would like to see
+            my resume please click on this link to download a pdf format of my
+            <a
+              className="blue-gradient_text font-semibold cursor-pointer"
+              href="https://storage.rxresu.me/cm3gf1q9j08jydbpo1omxza9k/resumes/Micah's Resume.pdf"
+            >
+              {""} resume. {""}
+            </a>
+          </p>
+        </div>
+      </div>
 
-			<hr className='border-slate-200' />
+      <hr className="border-slate-200" />
 
-			<CTA />
-		</section>
-	);
+      <CTA />
+    </section>
+  );
 };
 
 export default About;
